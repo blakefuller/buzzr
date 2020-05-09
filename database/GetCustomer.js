617b61aa284d79796ec18605047c4ce55daf94e0
Blake <bfuller3134@gmail.com>
2020-05-08 17:11:08 -0700

    created GetCustomer.js

---
+++ database/GetCustomer.js
@@ -0,0 +1,27 @@
+import AWS from '../AWS.config'
+const config = require('../AWS.config.json');
+
+let docClient = new AWS.DynamoDB.DocumentClient();
+
+async function GetCustomer(id) {
+
+    var params = {
+        TableName: config.restaurant,
+        Key: {
+            "customerID": id
+        }
+    };
+
+    return new Promise(resolve => {
+        docClient.get(params, function (err, data) {
+            if (err) {
+                console.log("error - " + JSON.stringify(err, null, 2));
+                resolve(false);
+            }
+            else {
+                console.log("success - " + JSON.stringify(data, null, 2));
+                resolve(true);
+            }
+        })
+    })
+}
